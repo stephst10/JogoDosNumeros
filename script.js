0975176c1c1302c007022c812db466c01fd1e050
stephst10 <stephaniesst10@gmail.com>
2024-10-07 10:20:02 -0300

    estilização

---
+++ script.js
@@ -0,0 +1,10 @@
+let numeroSecreto;
+let tentativas = 0;
+
+function gerarNumeroAleatorio() {
+    return new Promise((resolve) => {
+        setTimeout(() => {
+            numeroSecreto
+        }, 1000)
+    })
+}
