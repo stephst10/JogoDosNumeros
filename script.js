247537c885c086a90610cd3163f743d1e2a757a2
stephst10 <stephaniesst10@gmail.com>
2024-10-09 10:59:36 -0300

    finalização

--- script.js
+++ script.js
@@ -29,6 +29,28 @@ async function iniciarJogo() {
     await gerarNumeroAleatorio();
     
     const botaoEnviar = document.getElementById('enviar');
+    const inputPalpite = document.getElementById('palpite');
+    const resultado = document.getElementById('resultado');
+    const tentativasDisplay = document.getElementById('tentativas');
+
+    botaoEnviar.addEventListener('click', async () => {
+        try {
+            const palpite = parseInt(inputPalpite.value);
+            const mensagem = await verificarPalpite(palpite);
+            resultado.textContent = mensagem;
+            tentativasDisplay.textContent = `Tentativas: ${tentativas}`;
+            inputPalpite.value = '';
+
+            if (mensagem.includes('acertou')) {
+                tentativas = 0;
+                resultado.textContent += ' O jogo será reiniciado!';
+                await gerarNumeroAleatorio();
+                tentativasDisplay.textContent = "";
+            }
+        }  catch (erro) {
+            resultado.textContent = erro;
+        }
+    })
 }
 
 window.onload = iniciarJogo;
